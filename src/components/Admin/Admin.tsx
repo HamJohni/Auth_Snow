e1842d1b62ade137970042a41b90e8cc3208e225
keraemoe <weekera@gmail.com>
2023-05-05 19:01:29 +0600

    OK

---
+++ src/components/Admin/Admin.tsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import s from "./Admin.module.scss";
+
+const Admin = () => {
+  return (
+    <>
+      <div className={s.container}>
+        <div className={s.admin_page}>
+          <h1>
+            
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
