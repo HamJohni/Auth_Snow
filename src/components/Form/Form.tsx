a3cf48af915d3e9da6a7a3aa73445548c76b4de4
HamJohni <joni.pacan.wf@gmail.com>
2023-05-05 19:15:28 +0600

    Error

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,22 @@
 import auth from '../../pages/auth/auth.module.scss'
 import {AiOutlineUserAdd} from "react-icons/ai";
-import {HiOutlineMail} from "react-icons/hi";
-import {BsBriefcase} from "react-icons/bs";
-
+import {useForm} from "react-hook-form";
+import {IUser} from "@/models/FormType/FormType";
 
 const Form = () => {
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState:{errors}} = useForm<IUser>()
+
+    const registerUser = (data) => {
+        console.log(data)
+    }
+
     return(
-        <form className={auth.form}>
+        <form className={auth.form} noValidate onSubmit={handleSubmit(registerUser)}>
 
             <div className={auth.form__left}>
                 <span className={auth.form__left_circle}>
@@ -20,16 +30,16 @@ const Form = () => {
             <div className={auth.form__right}>
                 <div className={auth.form__right_content}>
                     <label className={auth.form__right_label}>
-                        {/*<span className={auth.form__right_icon}>*/}
-                        {/*    <HiOutlineMail/>*/}
-                        {/*</span>*/}
-                        <input type="text" className={auth.form__right_input} placeholder="Name"/>
+                        <input type="text"
+                               {...register('name', {
+                                   required: {
+                                       message: "required field" , value: true
+                                   }
+                               })}
+                               className={auth.form__right_input} placeholder="Name"/>
                     </label>
 
                     <label className={auth.form__right_label}>
-                        {/*<span className={auth.form__right_icon}>*/}
-                        {/*    <BsBriefcase/>*/}
-                        {/*</span>*/}
                         <select className={auth.form__right_input}>
                             <option className={auth.form__right_option}>Выберите должность:</option>
                             <option value="frontend" className={auth.form__right_option}>Frontend</option>
@@ -39,7 +49,7 @@ const Form = () => {
                     </label>
 
                     <div className={auth.form__box}>
-                        <button className={auth.form__box_btn}>Sign up</button>
+                        <button className={auth.form__box_btn} type="submit">Sign up</button>
                     </div>
                 </div>
             </div>
