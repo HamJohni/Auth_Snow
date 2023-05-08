896ba243d640c454b9175adab4fdaa26ec680a5a
keraemoe <weekera@gmail.com>
2023-05-08 19:39:47 +0600

    ok

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,10 +1,170 @@
-﻿import React from 'react';
+﻿import React, { useState } from "react";
+import s from "./Admin.module.scss";
+
+import {
+  SimpleGrid,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  Text,
+  CardFooter,
+  Button,
+  Wrap,
+  WrapItem,
+  Avatar,
+  Stack,
+  Box,
+} from "@chakra-ui/react";
+
+
+interface User {
+  id: number;
+  name: string;
+  job: string;
+  img: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: "Kera Yan",
+    job: "Frontend Developer",
+    img: "https://bit.ly/dan-abramov",
+  },
+  {
+    id: 2,
+    name: "Belek Belekov",
+    job: "Senior Mom Deviloper",
+    img: "https://bit.ly/tioluwani-kolawole",
+  },
+  {
+    id: 4,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/kent-c-dodds",
+  },
+  {
+    id: 4,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/ryan-florence",
+  },
+  {
+    id: 5,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/prosper-baba",
+  },
+  {
+    id: 6,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/code-beast",
+  },
+  {
+    id: 7,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/sage-adebayo",
+  },
+  {
+    id: 8,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/sage-adebayo",
+  },
+  {
+    id: 9,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/code-beast",
+  },
+  {
+    id: 10,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/ryan-florence",
+  },
+  {
+    id: 11,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/sage-adebayo",
+  },
+  {
+    id: 12,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/code-beast",
+  },
+  {
+    id: 13,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/ryan-florence",
+  },
+  {
+    id: 14,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/sage-adebayo",
+  },
+  {
+    id: 15,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/code-beast",
+  },
+  {
+    id: 16,
+    name: "Chort Chortov",
+    job: "Python Developer",
+    img: "https://bit.ly/ryan-florence",
+  },
+];
 
 const Admin = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={s.container}>
+        <div className={s.panel}>
+          <SimpleGrid
+            spacing={4}
+            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+          >
+            {users.map((item) => (
+              <Card>
+                <CardHeader>
+                  <Wrap>
+                    <WrapItem>
+                      <Avatar name="Dan Abrahmov" src={item.img} />
+                    </WrapItem>
+                  </Wrap>
+                  <Heading size="md">{item.name}</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>{item.job}</Text>
+                </CardBody>
+                <CardFooter>
+                  <Button>Select</Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </SimpleGrid>
+          <div className={s.under}>
+            <Stack direction="row" spacing={4} align="end">
+              <Button colorScheme="teal" variant="solid">
+                Отметить
+              </Button>
+              <Button colorScheme="red" variant="outline">
+                Не пришел
+              </Button>
+            </Stack>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/store/reducers/developers.ts
+++ src/store/reducers/developers.ts
@@ -1,69 +1,49 @@
-<<<<<<< HEAD
 import { IUser } from "@/models/FormType/FormType";
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState: IUser = {
-  developers: [],
-  isLoading: false,
-  error: "",
-};
-
-export const developers = createSlice({
-  name: "developers",
-  initialState,
-  reducers: {},
-});
-
-export default developers.reducer;
-=======
-import {IUser} from "@/models/FormType/FormType";
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getAllDevelopers = createAsyncThunk(
-    'developers/getAllDevelopers',
-    async (_, developersApi) => {
-        try {
-            const res = await axios.get('http://localhost:4080/developers')
+  "developers/getAllDevelopers",
+  async (_, developersApi) => {
+    try {
+      const res = await axios.get("http://localhost:4080/developers");
 
-            return res.data
-
-        } catch (e) {
-            return developersApi.rejectWithValue("Не удалось получить разработчиков")
-        }
+      return res.data;
+    } catch (e) {
+      return developersApi.rejectWithValue("Не удалось получить разработчиков");
     }
-)
+  }
+);
 
 interface developersState {
-    developers: IUser[],
-    status: string,
-    error: string
+  developers: IUser[];
+  status: string;
+  error: string;
 }
 
 const initialState: developersState = {
-    developers: [],
-    status: '',
-    error: ''
-}
+  developers: [],
+  status: "",
+  error: "",
+};
 
 const developers = createSlice({
-    name: 'developers',
-    initialState,
-    reducers: {},
-    extraReducers: {
-        [getAllDevelopers.rejected]: (state, action) => {
-            state.error = action.payload
-            state.status = 'error'
-        },
-        [getAllDevelopers.pending]: (state) => {
-            state.status = 'loading'
-        },
-        [getAllDevelopers.fulfilled]: (state, action) => {
-            state.status = true
-            state.developers = action.payload
-        }
-    }
-})
+  name: "developers",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getAllDevelopers.rejected]: (state, action) => {
+      state.error = action.payload;
+      state.status = "error";
+    },
+    [getAllDevelopers.pending]: (state) => {
+      state.status = "loading";
+    },
+    [getAllDevelopers.fulfilled]: (state, action) => {
+      state.status = true;
+      state.developers = action.payload;
+    },
+  },
+});
 
 export default developers.reducer;
->>>>>>> 6845b5e99f8d8ae8ff08da4b431ca39b6188c431
